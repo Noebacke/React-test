3e63a352269ef0c2f002e38aaeb8252ece985c57
Noé <noeebacke@gmail.com>
2022-12-16 16:53:47 +0100

    update little bit css

--- src/components/cats/ShowCats.jsx
+++ src/components/cats/ShowCats.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import ShowCatsDetails from "./ShowcatsDetails";
+import './cats.css';
 
 
 const ShowCats = ()=>{
--- src/components/meal/ShowPartOfMeal.jsx
+++ src/components/meal/ShowPartOfMeal.jsx
@@ -1,4 +1,5 @@
 import { Link, } from "react-router-dom";
+import './PartOfMeal.css'
 
 
 const ShowPartOfMeal = (props)=>{
